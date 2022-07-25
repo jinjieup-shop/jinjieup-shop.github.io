c04fdc98a453ebbe152dc74d6be191bad250e874
lipeilin375 <lipeilin@jinjieup.onmicrosoft.com>
2022-07-25 17:11:09 +0800

    v1.1.3

--- know.js
+++ know.js
@@ -6,7 +6,7 @@ window.onload = function(){
   for (let i = 0; i < Agents.length; i++) {
     if (u.indexOf(Agents[i]) > 0) {
       flag = false;
-      window.open("http://www.jinjieup.tk/phone/index.html", "_self");
+      window.open("https://shop.jinjieup.tk/phone/index.html", "_self");
     }
     else {
       flag = true;
